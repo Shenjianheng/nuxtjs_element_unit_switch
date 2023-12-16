8778f1bda9b6263b5ded43888085db9876460f9f
shenjianheng1114 <774921030@qq.com>
2023-12-16 23:05:06 +0800

    再次修改nuxt.config.js文件配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/head'
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
