1e1041b8f63688b110d6384c8a2897fa3637f3a6
shenjianheng1114 <774921030@qq.com>
2023-12-16 23:36:57 +0800

    更改nuxt.config.js的title配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxtjs_element_unit_switch',
+    title: 'px | vw | rem | unit converstion',
     htmlAttrs: {
       lang: 'en',
     },
