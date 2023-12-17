df8a6d5089e7cdadf02c16d079e1337bdc1cb4db
shenjianheng1114 <774921030@qq.com>
2023-12-17 10:44:55 +0800

    nuxt.config.js添加plausible数据统计配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://plausible.io/js/script.js',
+        async: true,
+        defer: true,
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
